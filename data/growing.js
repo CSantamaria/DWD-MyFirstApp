8301ad5421f8a567084f9944a3c8b4a38beccb24
CSantamaria <clara.j.santamaria@gmail.com>
2014-04-09 12:57:03 -0400

    Create growing.js

---
+++ data/growing.js
@@ -0,0 +1,12 @@
+exports.data = [
+  {
+    'url': 'Fruits and vegetables por toda la región',
+    'name': 'F&V',
+    'Tomato/Tomatoe': [
+      'Potatoes',
+      'Blueberries',
+      'Tribbles'
+    ],
+      'input': 'I have no idea what I'm doing'
+  }
+];
